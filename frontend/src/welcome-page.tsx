3e6e7c2477e00690906d3131907e09f3fbf81439
HampusLarss0n <hlarsson0616@gmail.com>
2025-10-29 22:46:22 +0100

    Add welcome page

---
+++ frontend/src/welcome-page.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function FormPropsTextFields() {
+  return (
+    <Box
+      component="form"
+      sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Name"
+        />
+                <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="email"
+        />
+                <TextField
+          required
+          id="outlined-required"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
+      </div>
+    </Box>
+  );
+}
