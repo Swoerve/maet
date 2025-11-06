169205d6452f43bc1b04c08e56ee5d95da03dd36
swoerve <swoerve@gmail.com>
2025-11-06 22:29:19 +0100

    basic route structure

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,22 @@
 //import express = require('express')
 import express from 'express'
 import cors from 'cors'
+import userRoutes from './routes/user.ts'
+import boardRoutes from './routes/board.ts'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+// default route to test
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
+
+// routes
+app.use('/user', userRoutes)
+app.use('/board', boardRoutes)
+
 
 export default app
