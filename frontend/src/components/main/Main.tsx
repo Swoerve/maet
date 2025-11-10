79fc81cc2d1ac742d1f0499902b1c111a563ae45
swoerve <swoerve@gmail.com>
2025-11-10 14:49:36 +0100

    added folder structure, added base main site

---
+++ frontend/src/components/main/Main.tsx
@@ -0,0 +1,111 @@
+import { useEffect, useState } from "react";
+import "./Main.css";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import axios from "axios";
+
+function Main() {
+
+  const [boards, setBoards] = useState<Record<string, unknown>[]>([]);
+  const [newModalOpen, setNewModalOpen] = useState<boolean>(false);
+  const handleOpen = () => setNewModalOpen(true);
+  const handleClose = () => setNewModalOpen(false);
+  const [newBoardTitle, setNewBoardTitle] = useState<string>("");
+
+  const user = sessionStorage.getItem("user");
+  if(user === null){
+      console.log('user not Logged in')
+      // kick user back to login site
+  }
+
+  useEffect(() => {
+    async function getUserBoards() {
+      try {
+        const response = await axios.get(`/api/boards/user/${user}`);
+        console.log(response);
+        const data = await response.data;
+        console.log(data);
+        setBoards(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if (boards.length == 0) {
+      getUserBoards();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const boardItems = boards.map((board, index) => (
+    <>
+      <Grid key={index}>
+        <Card>
+          <CardActionArea>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {String(board.title)}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button>Go</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </>
+  ));
+
+  async function createNewBoard() {
+    const response = await axios.post(`/api/boards`, {
+      owner_id: user,
+      title: newBoardTitle
+    });
+    console.log(response);
+  }
+
+  return (
+    <>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {boardItems}
+      </Grid>
+      <Button onClick={handleOpen}>New Board</Button>
+      <Modal
+        open={newModalOpen}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Create new Board
+          </Typography>
+          <TextField
+            id="new-board-title"
+            label="Title"
+            variant="outlined"
+            value={newBoardTitle}
+            onChange={(event) => {
+              setNewBoardTitle(event.target.value);
+            }}
+          />
+          <Button onClick={createNewBoard}>Create</Button>
+        </Box>
+      </Modal>
+    </>
+  );
+}
+
+export default Main;
