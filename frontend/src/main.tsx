2caee54f7449ad6f85b55ff172c5331133db7aed
HampusLarss0n <hlarsson0616@gmail.com>
2025-11-10 13:56:03 +0100

    welcome page created

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter} from 'react-router'
 import { StyledEngineProvider } from '@mui/material/styles'
 import GlobalStyles from '@mui/material/GlobalStyles'
 import './index.css'
 import App from './App.tsx'
+import Login from './login-page.tsx'
 
+const router = createBrowserRouter([
+  {path: '/login', element: <Login/>}
+])
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <StyledEngineProvider enableCssLayer>
       <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+        <RouterProvider router={router}/>
         <App />
     </StyledEngineProvider>
   </StrictMode>,
