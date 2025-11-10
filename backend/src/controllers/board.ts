f387a3d5b53411b7f0bdff639aa1b1942dc90a00
swoerve <swoerve@gmail.com>
2025-11-10 09:36:57 +0100

    board controllers

--- backend/src/controllers/board.ts
+++ backend/src/controllers/board.ts
@@ -5,3 +5,25 @@ import type { Request, Response } from 'express';
 export function helloBoard(req: Request, res: Response): string {
   return fetchHelloBoard();
 }
+
+export function postBoard(req: Request, res: Response){
+  res.send(`creating new board`);
+}
+
+export function patchBoard(req: Request, res: Response){
+  res.send(`editing board with id ${req.params.id}`);
+}
+
+export function getBoard(req: Request, res: Response){
+  res.send(`getting board with id ${req.params.id}`);
+}
+
+export function getUserBoards(req: Request, res: Response){
+  res.send(`getting board of user with id ${req.params.id}`);
+}
+
+export function postUserBoardConnection(req: Request, res: Response){
+  res.send(`adding user with id ${req.params.id} to board with id ${req.params.id}`);
+}
+
+
