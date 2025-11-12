24e492385c6d5afabb85efb395d26e8bd36ddc74
swoerve <swoerve@gmail.com>
2025-11-12 17:10:57 +0100

    gdpr checkbox and input feedbacks

--- frontend/src/components/login/login-page.tsx
+++ frontend/src/components/login/login-page.tsx
@@ -1,150 +1,254 @@
-
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router';
-import { Typography } from '@mui/material';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormHelperText,
+  Typography,
+} from "@mui/material";
 
 export default function LoginPage() {
-  const navigate = useNavigate()
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [isSignup, setIsSignup] = useState(false)
+  const [isSignup, setIsSignup] = useState(false);
 
+  const [gdpr, setGdpr] = useState(false)
+
+
+  const [usernameError, setUsernameError] = useState(false)
+  const [emailError, setEmailError] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+  const [gdprError, setGdprError] = useState(false)
+
+
+  useEffect(()=> {
+    setUsername("")
+    setPassword("")
+    setEmail("")
+    setGdpr(false)
+  }, [isSignup])
 
   // logs the user in by sending a get to the api,
   // checking if the email and password match and are part of the same account
   // then sending us out to /main
-  function loginUser() {
-    axios.post(`/api/user/verify`, {
-      email: email,
-      password: password
-    }).then((res) => res.data).then((data) => {
-      if(data){
-        sessionStorage.setItem('user', data.id)
-        navigate('/main')
-      }
-    })
+  function loginUser(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    console.log("signing up")
+    if(emailError && passwordError){
+      console.log("not valid")
+      return
+    }
+    axios
+      .post(`/api/user/verify`, {
+        email: email,
+        password: password,
+      })
+      .then((res) => res.data)
+      .then((data) => {
+        if (data) {
+          sessionStorage.setItem("user", data.id);
+          navigate("/main");
+        }
+      });
+  }
+
+  function isValidForm(): boolean{
+    if(gdpr && !usernameError && !emailError && !passwordError){
+      return true
+    }
+    return false
   }
 
   // signs up a new user
   // saves the new users id locally
   // then sends us out to /main
-  function signupUser() {
-        axios.post(`/api/user`, {
+  function signupUser(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    console.log("signing up")
+    if(!isValidForm()){
+      console.log("not valid")
+      return
+    }
+    axios
+      .post(`/api/user`, {
         username: username,
         email: email,
-        password: password
-    }
-    ).then((res) => res.data).then((data) => {
-      if(data){
-        sessionStorage.setItem('user', data.id)
-        navigate('/main')
-      }
-    })
+        password: password,
+      })
+      .then((res) => res.data)
+      .then((data) => {
+        if (data) {
+          sessionStorage.setItem("user", data.id);
+          navigate("/main");
+        }
+      });
   }
 
   return (
     <Box
-    display="flex"
-    justifyContent="center"
-    alignItems="center"
-    minHeight="90vh">
-      {!isSignup ? 
-      <div>
-        <Grid container spacing={2} maxWidth="sm">
-          <Stack spacing={2}>
-            <Typography variant='h2' gutterBottom>Login</Typography>
-            <Box
-              component="form"
-              sx={{ '& .MuiTextField-root': { m: 1, width: '40ch' } }}
-              noValidate
-              autoComplete="off"
-            >
-              <Grid size={3}>
-                <TextField
-                  required
-                  id="outlined-required"
-                  label="email"
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="90vh"
+    >
+      {!isSignup ? (
+        <div>
+          <Grid container spacing={2} maxWidth="sm">
+            <Stack spacing={2}>
+              <Typography variant="h2" gutterBottom>
+                Login
+              </Typography>
+              <form onSubmit={loginUser}>
+
+              <FormControl>
+                <FormGroup>
+                  <TextField
+                    required
+                    id="outlined-required"
+                    label="email"
+                    value={email}
+                    onChange={(event) => {setEmail(event.target.value); setEmailError(email.length < 7)}}
+                    sx={{
+                      maxWidth: "40ch",
+                      minWidth: "35ch",
+                      marginTop: "1rem",
+                    }}
+                    error={emailError}
+
                   />
-              </Grid>
-              <Grid size={3}>
-                <TextField
-                  required
-                  id="outlined-required"
-                  label="Password"
-                  type="password"
-                  autoComplete="current-password"
-                  value={password}
-                  onChange={(event) => setPassword(event.target.value)}
+                  <TextField
+                    required
+                    id="outlined-required"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={(event) => {setPassword(event.target.value); setPasswordError(password.length < 5)}}
+                    sx={{
+                      maxWidth: "40ch",
+                      minWidth: "35ch",
+                      marginTop: "1rem",
+                    }}
+                    error={passwordError}
                   />
-              </Grid>
-            </Box>
-            <Stack direction="row" spacing={2} sx={{justifyContent: "space-evenly", alignItems: "center"}}>
-              <Button size='large' variant="outlined" onClick={loginUser}>login</Button>
-              <Button size='large' variant="outlined" onClick={() => setIsSignup(true)}>sign up?</Button>
+                </FormGroup>
+              </FormControl>
+              <Stack
+                direction="row"
+                spacing={2}
+                sx={{ justifyContent: "space-evenly", alignItems: "center", marginTop: ".5rem"}}
+              >
+                <Button
+                  size="large"
+                  variant="outlined"
+                  onClick={() => setIsSignup(true)}
+                >
+                  Sign Up?
+                </Button>
+                <Button type="submit" size="large" variant="outlined">
+                  Log In
+                </Button>
+              </Stack>
+              </form>
             </Stack>
-            
-          </Stack>
-        </Grid>
-      </div> 
-      :
-      <div>
-        <Grid container spacing={2}>
-          <Stack spacing={2}>
-            <Typography variant='h2' gutterBottom>Sign up</Typography>
-            <Box
-              component="form"
-              sx={{ '& .MuiTextField-root': { m: 1, width: '40ch' } }}
-              noValidate
-              autoComplete="off"
-            >
-            <Grid size={3}>
-              <TextField
-                required
-                id="outlined-required"
-                label="username"
-                value={username}
-                onChange={(event) => setUsername(event.target.value)}
-                />
-            </Grid>
-            <Grid size={3}>
-              <TextField
-                required
-                id="outlined-required"
-                label="email"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-                />
-            </Grid>
-            <Grid size={3}>
-              <TextField
-                required
-                id="outlined-required"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-                />
-            </Grid>
-            </Box>
-            <Stack direction="row" spacing={2} sx={{justifyContent: "space-evenly", alignItems: "center"}}>
-            <Button size='large' variant="outlined" onClick={signupUser}>sign up</Button>
-            <Button size='large' variant="outlined" onClick={() => setIsSignup(false)}>back</Button>
+          </Grid>
+        </div>
+      ) : (
+        <div>
+          <Grid container spacing={2}>
+            <Stack spacing={2}>
+              <Typography variant="h2" gutterBottom>
+                Sign up
+              </Typography>
+              <form onSubmit={signupUser}>
+
+              <FormControl>
+                <FormGroup>
+                  <TextField
+                    required
+                    id="outlined-required"
+                    label="username"
+                    value={username}
+                    onChange={(event) => {setUsername(event.target.value); setUsernameError(username.length < 5)}}
+                    sx={{
+                      maxWidth: "40ch",
+                      minWidth: "35ch",
+                      marginTop: "1rem",
+                    }}
+                    error={usernameError}
+                  />
+
+                  <TextField
+                    required
+                    id="outlined-required"
+                    label="email"
+                    value={email}
+                    onChange={(event) => {setEmail(event.target.value); setEmailError(email.length < 7)}}
+                    sx={{
+                      maxWidth: "40ch",
+                      minWidth: "35ch",
+                      marginTop: "1rem",
+                    }}
+                    error={emailError}
+                  />
+                  <TextField
+                    required
+                    id="outlined-required"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={(event) => {setPassword(event.target.value); setPasswordError(password.length < 5)}}
+                    sx={{
+                      maxWidth: "40ch",
+                      minWidth: "35ch",
+                      marginTop: "1rem",
+                    }}
+                    error={passwordError}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <FormControlLabel
+                    required
+                    control={<Checkbox checked={gdpr} onChange={(event)=>{setGdpr(event.target.checked); setGdprError(!gdpr)}} />}
+                    label="You consent to us saving your personal data in conjunction to GDPR laws"
+                    sx={{maxWidth: "40ch"}}
+                  />
+                  <FormHelperText error={!gdprError}>You must consent to sign up</FormHelperText>
+                </FormGroup>
+              </FormControl>
+              <Stack
+                direction="row"
+                spacing={2}
+                sx={{ justifyContent: "space-evenly", alignItems: "center", marginTop: ".5rem"}}
+              >
+                <Button
+                  size="large"
+                  variant="outlined"
+                  onClick={() => setIsSignup(false)}
+                >
+                  back
+                </Button>
+                <Button type="submit" size="large" variant="outlined">
+                  sign up
+                </Button>
+              </Stack>
+              </form>
             </Stack>
-          </Stack>
-        </Grid>
-      </div>
-      }
-  </Box>
+          </Grid>
+        </div>
+      )}
+    </Box>
   );
 }
